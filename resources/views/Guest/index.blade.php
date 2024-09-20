ab93e6c64215320eb9a8b63b7010dacc17ba5692
Menichetti Simone <simon3menichetti95@gmail.com>
2024-09-20 14:52:49 +0200

    1.002 Feat:( add Dash controller/ group all route Admin with middleware & auth, verified/
    FIX:( RouteProvider in 'admin'/
    
    Feat:(add new folder in view Admin and add in DashControll view admin.index and new view Admin/index.blade
    
    Feat:(add new controller for Guest / add new folder in view/Guest and add nw index.blade extend guest.index/
    
    Fix:(Layout/Guest.blade deleted all and add html @yeld)
    
    Feat:(add new route Guest in web.php and return view in PageController Guest.index )

---
+++ resources/views/Guest/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.Guest')
+
+@section('content')
+    <h1>Benvenuto nella pagina Guest</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashController;
+use App\Http\Controllers\Guest\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +30,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', [DashController::class, 'index'])->name('home');
+    });
+
+require __DIR__ . '/auth.php';
