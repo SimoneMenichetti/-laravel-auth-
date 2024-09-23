6e5c8b10224bb7e1972c165c95281ebf4f9f757f
Simone Menichetti <simon3menichetti95@gmail.com>
2024-09-23 18:00:57 +0200

    feat:(add route in aside for Posts & return view in PostController )

--- resources/views/Admin/partials/aside.blade.php
+++ resources/views/Admin/partials/aside.blade.php
@@ -1,7 +1,7 @@
 <aside class="text-bg-dark">
     <ul>
         <li><a href="{{ route('admin.home') }}">home</a></li>
-        <li><a href="#">elenco post</a></li>
+        <li><a href="{{ route('admin.posts.index') }}">elenco post</a></li>
         <li><a href="#">nuovo post</a></li>
     </ul>
 </aside>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\DashController;
 use App\Http\Controllers\Guest\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,6 +39,7 @@ Route::middleware(['auth', 'verified'])
     ->name('admin.')
     ->group(function () {
         Route::get('/', [DashController::class, 'index'])->name('home');
+        Route::resource('posts', PostController::class);
     });
 
 require __DIR__ . '/auth.php';
